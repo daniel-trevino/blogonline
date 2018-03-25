aa9a844a273f994d4f3ec1ba97fcf41169a5e084
Daniel Treviño <danielivert@gmail.com>
2018-03-25 16:16:28 +0200

    Add HeroHeader component

---
+++ src/components/organisms/HeroHeader.js
@@ -0,0 +1,24 @@
+import React from "react";
+import RaisedButton from "material-ui/RaisedButton";
+import ImageBackground from "../atoms/ImageBackground";
+import Image from "../../images/stockholm.jpg";
+
+export default () => (
+  <div className="heroHeader">
+    <ImageBackground classType="heroHeader__backgroundImage" src={Image} />
+    <div className="heroHeader__content">
+      <div className="heroHeader__title">
+        <h1>Welcome to blogonline!</h1>
+        <h2>The place where you can read different taxi stories</h2>
+      </div>
+
+      <div className="heroHeader__button-container">
+        <RaisedButton
+          className="heroHeader__button"
+          label="Create a post"
+          primary
+        />
+      </div>
+    </div>
+  </div>
+);
--- src/components/pages/Frontpage.js
+++ src/components/pages/Frontpage.js
@@ -1,9 +1,11 @@
 import React from "react";
-import Navigation from "../organisms/Navigation";
 import Body from "../molecules/Body";
+import Navigation from "../organisms/Navigation";
+import HeroHeader from "../organisms/HeroHeader";
 
 export default () => (
   <Body>
     <Navigation />
+    <HeroHeader />
   </Body>
 );
